12e50c682b142062ab2fd62651cc0d2fbb6f9be5
xkxq <295953863@qq.com>
2019-06-03 10:31:16 +0800

    start

---
+++ src/main/java/com/xq/qiang/QiangApplication.java
@@ -0,0 +1,13 @@
+package com.xq.qiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xq/qiang/QiangApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xq.qiang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QiangApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
